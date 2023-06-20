00c9a45a5f99162511e40ec681169246aa600ab9
mohosinmiah <hamza1610330816@gmail.com>
2023-06-20 19:45:15 +0600

    Project Initial Setup

---
+++ src/main/java/com/testcontrollerlayer/testcontrollerlayer/TestcontrollerlayerApplication.java
@@ -0,0 +1,13 @@
+package com.testcontrollerlayer.testcontrollerlayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcontrollerlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcontrollerlayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testcontrollerlayer/testcontrollerlayer/TestcontrollerlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testcontrollerlayer.testcontrollerlayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontrollerlayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
