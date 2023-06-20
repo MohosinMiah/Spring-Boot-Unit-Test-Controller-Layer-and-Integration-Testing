dd8ef7c90f83f6daa054db5369577de8cd0290a4
mohosinmiah <hamza1610330816@gmail.com>
2023-06-21 05:51:34 +0600

    Unit test for all employees rest api

--- src/test/java/com/testcontrollerlayer/testcontrollerlayer/controller/EmployeeControllerTests.java
+++ src/test/java/com/testcontrollerlayer/testcontrollerlayer/controller/EmployeeControllerTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.mockito.ArgumentMatcher;
 import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
+import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -16,6 +17,9 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static  org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.hamcrest.CoreMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -66,4 +70,44 @@ public class EmployeeControllerTests {
             .andExpect(MockMvcResultMatchers.jsonPath("$.lastName", CoreMatchers.is(employee.getLastName())))
             .andExpect(MockMvcResultMatchers.jsonPath("$.email", CoreMatchers.is(employee.getEmail())));
     }
+
+    // Unit test for all employees rest api
+    @Test
+    public void givenListOfEmployees_whenGetAll_thenReturnEmployeeList() throws JsonProcessingException, Exception {
+
+        // Given: Setup object or precondition
+         Employee employee1 = Employee.builder()
+                .firstName("MOHOSIN")
+                .lastName("MIAH")
+                .email("mohosinmiah1610@gmail.com")
+                .departmentCode("CSE")
+                .build();
+        
+        Employee employee2 = Employee.builder()
+                .firstName("John")
+                .lastName("Doe")
+                .email("johndoe@example.com")
+                .departmentCode("IT")
+                .build();
+        
+        // Create two separate ArrayLists of employees
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(employee1);
+        employeeList.add(employee2);
+
+        BDDMockito.given(employeeService.getEmployeeList())
+                .willReturn(employeeList);
+
+        // When: Action or behavior that we are going to test
+        ResultActions response =  mockMvc.perform( get("/api/employees") );
+
+        // Then: Verify the output or expected result
+
+        response
+            .andDo(MockMvcResultHandlers.print())   // Display The API Request Details -  Very help full for debugging 
+            .andExpect(MockMvcResultMatchers.status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.size()", CoreMatchers.is(employeeList.size())));
+    }
+
+
 }
