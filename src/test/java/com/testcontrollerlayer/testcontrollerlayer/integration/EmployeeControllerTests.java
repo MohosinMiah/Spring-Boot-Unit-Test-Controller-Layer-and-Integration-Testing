e58a509c816d0633598c3b29378fcb353942d356
mohosinmiah <hamza1610330816@gmail.com>
2023-06-22 20:34:59 +0600

    Integration test create employees rest api

--- src/test/java/com/testcontrollerlayer/testcontrollerlayer/integration/EmployeeControllerTests.java
+++ src/test/java/com/testcontrollerlayer/testcontrollerlayer/integration/EmployeeControllerTests.java
@@ -1,14 +1,26 @@
 package com.testcontrollerlayer.testcontrollerlayer.integration;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.testcontrollerlayer.testcontrollerlayer.entity.Employee;
 import com.testcontrollerlayer.testcontrollerlayer.repository.EmployeeRepository;
+
+import static  org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @SpringBootTest(
     webEnvironment = WebEnvironment.RANDOM_PORT
@@ -31,5 +43,32 @@ public class EmployeeControllerTests {
         employeeRepository.deleteAll();
     }
 
+
+    // Integration test create employees rest api
+    @Test
+    public void givenEmployeeObject_whenCreateEmployee_thenReturnSavedEmployee() throws JsonProcessingException, Exception {
+
+        // Given: Setup object or precondition
+            Employee employee = Employee.builder()
+                .firstName("MOHOSIN")
+                .lastName("MIAH")
+                .email("mohosinmiah1610@gmail.com")
+                .departmentCode("CSE")
+                .build();
+
+        // When: Action or behavior that we are going to test
+        ResultActions response =  mockMvc.perform( post("/api/employees").contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(employee))
+        );
+
+        // Then: Verify the output or expected result
+
+        response
+            .andDo(MockMvcResultHandlers.print())   // Display The API Request Details -  Very help full for debugging 
+            .andExpect(MockMvcResultMatchers.status().isCreated())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.firstName", CoreMatchers.is(employee.getFirstName())))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.lastName", CoreMatchers.is(employee.getLastName())))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.email", CoreMatchers.is(employee.getEmail())));
+    }
 
 }
